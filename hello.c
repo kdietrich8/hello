3a2d3d433bb061d571fb4fb4175d67852cc5dc1e
Teighan <tpeterson26@huskers.unl.edu>
2021-08-26 20:16:12 +0000

    added stuff to code

--- hello.c
+++ hello.c
@@ -12,6 +12,8 @@ int main ( int argc, char **argv) {
 
     printf("Koen D --\n");
     printf("Computer Engineering\n");
+    printf("Teighan\n");
+    printf("Computer Science\n");
 
     return 0;
 
